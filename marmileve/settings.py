f263627f47cdd56df4ae02320edc3019805b4b52
ETLopes <eduardotorreslopes@gmail.com>
2018-09-09 07:08:20 -0300

    Heroku Ready

--- marmileve/settings.py
+++ marmileve/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['192.168.0.113', '192.168.1.102', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
